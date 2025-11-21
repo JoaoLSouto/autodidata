0d2db610c798efa79b0f110a3fa270c74753befe
joaolsouto <contato.joaolsouto@gmail.com>
2025-11-20 22:12:45 -0300

    first anwers

--- containers/challenge.py
+++ containers/challenge.py
@@ -1,13 +1,23 @@
 # 1 - Crie uma lista de musicos
+artists = ["Travis Scott", "Lady Gaga", "Drake"]
 
 # 2- Crie uma lista de tuplas, com cada tupla, contendo a longitude e a latitude de algum lugar onde você visitou
+tvtower = ("-15.699244", "-47.829556" )
 
 # 3 - Crie um dicionário contendo diferentes atributos sobre você: altura, cor favorita, autor favorito etc
-
+about_me = {"weight":
+            "1.73",
+            "favorite_color":
+            "gray",
+            "Favorite_author":
+            "Bauman",    
+            }
 # 4 - Escreva um programa que permita que o usuário pergunte sua altura, cor favorita ou autor favorito e retorne 
 # o resultado a partir do dicionário criado no desafio anterior
 
-#5 rie um dicionário que mapeie seus músicos favoritos para uma lista das canções que você mais gosta dele
+#5 Crie um dicionário que mapeie seus músicos favoritos para uma lista das canções que você mais gosta dele
 
 #6 As listas, as tuplas e os dicionários são apenas alguns dos containers internos do Python. Faça uma pesquisa sobre 
 # os conjuntos (um tipo de container) do Python. Quando você usaria um conjunto?
+
+# tinyurl.com/z54w9cb
