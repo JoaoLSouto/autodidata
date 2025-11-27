ca122a2a26cd7a5f0032b097c64c652b08edf7db
joaolsouto <contato.joaolsouto@gmail.com>
2025-11-27 15:08:20 -0300

    modules

---
+++ modules/challenge1.py
@@ -0,0 +1,7 @@
+import statistics
+
+data = [1, 3, 5, 7, 9]
+
+media = statistics.median_low(data)
+
+print(media)
---
+++ modules/challenge2.py
@@ -0,0 +1,7 @@
+import cubed
+
+answer = int(input("insira um valor para ser elevado ao cubo: "))
+
+x = cubed.cubed_function(answer)
+
+print(x)
