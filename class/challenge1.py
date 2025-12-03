a108e08121b1bf8644bf16e4a2cfa65e85bddde1
joaolsouto <contato.joaolsouto@gmail.com>
2025-12-02 23:43:02 -0300

    add challenge class

---
+++ class/challenge1.py
@@ -0,0 +1,6 @@
+class Apple:
+    def __init__(self, origin, color, weight, variety):
+        self.origin = origin
+        self.color = color
+        self.weight = weight
+        self.variety = variety
